241da8b63fdf814d30467aae5be8b201275b707f
taskger <moswit7@gmail.com>
2024-02-09 17:16:07 +0700

    .

--- clients/src/navbar/HeaderEdu.js
+++ clients/src/navbar/HeaderEdu.js
@@ -5,7 +5,7 @@ const HeaderEdu = () => {
   return (
     <header className="header">
       <img src="/noti.png" alt="Notification Icon" className="notification-icon" />
-      <span className="Ed-text">ฝ่ายการศึกษา.</span>
+      <span className="Ed-text">ฝ่ายการศึกษา</span>
       <img src="/logout.png" alt="Logout Icon" className="logout-icon" />
     </header>
   );
