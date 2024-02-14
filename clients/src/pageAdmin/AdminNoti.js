98b68bfb3f122bc6fb8ff6efc6f67f2c38eca47c
Phitchayp <157294602+Phitchayp@users.noreply.github.com>
2024-02-14 20:54:06 +0700

    navbar

--- clients/src/pageAdmin/AdminNoti.js
+++ clients/src/pageAdmin/AdminNoti.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import './AdminNoti.css'
 import NavbarAdmin from "../navbar/NavbarAdmin";
 import HeaderAdmin from "../navbar/HeaderAdmin";
+import Boxnoti from "../oanchisa/Boxnoti";
+
 
 function AdminNoti() {
   return (
     <div>
-      <NavbarAdmin />
-      <HeaderAdmin />
+      {/* <NavbarAdmin />
+      <HeaderAdmin /> */}
+      <div className='Boxnoti_left'>
+        {/* <Boxnoti /> */}
+      </div>
+      
     </div>
   );
 }
