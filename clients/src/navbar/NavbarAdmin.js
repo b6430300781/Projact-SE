66017db6f66adb958a3241a659995ff5fe57ddc0
Phitchayp <157294602+Phitchayp@users.noreply.github.com>
2024-02-15 16:46:14 +0700

    navbar update

--- clients/src/navbar/NavbarAdmin.js
+++ clients/src/navbar/NavbarAdmin.js
@@ -5,30 +5,32 @@ import { Link } from 'react-router-dom';
 const NavbarAdmin = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <img src="profile.png" alt="" />
-        <span>:admin #####</span>
-      </div>
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <img src="index.png" alt="index" />
-          <Link to="/" className="nav-link">
-            ประกาศ
-          </Link>
-        </li>
-        <li className="nav-item">
-          <img src="time.png" alt="time" />
-          <Link to="/กำหนดการ" className="nav-link">
-            กำหนดการ
-          </Link>
-        </li>
-        <li className="nav-item">
-          <img src="user.png" alt="user" />
-          <Link to="/ข้อมูลผู้ใช้งาน" className="nav-link">
-            ข้อมูลผู้ใช้งาน
-          </Link>
-        </li>
-      </ul>
+     
+        <div className="navbar-left">
+          <img src="profile.png" alt="" />
+          <span>:admin #####</span>
+        </div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <img src="index.png" alt="index" />
+            <Link to="/" className="nav-link">
+              ประกาศ
+            </Link>
+          </li>
+          <li className="nav-item">
+            <img src="time.png" alt="time" />
+            <Link to="/กำหนดการ" className="nav-link">
+              กำหนดการ
+            </Link>
+          </li>
+          <li className="nav-item">
+            <img src="user.png" alt="user" />
+            <Link to="/ข้อมูลผู้ใช้งาน" className="nav-link">
+              ข้อมูลผู้ใช้งาน
+            </Link>
+          </li>
+        </ul>
+
     </nav>
   );
 };
