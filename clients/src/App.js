a01d40fc69b0f6c6db0d605b229eb2e73f859240
oanchisa <oanchisa.n@ku.th>
2024-02-16 12:30:49 +0700

    BoxNoti2

--- clients/src/App.js
+++ clients/src/App.js
@@ -12,35 +12,37 @@ import EduNoti from './pageEdu/EduNoti';
 import RegisResults from './pageEdu/RegisResults';
 import CoursesTaught from './pageEdu/CoursesTaught';
 import AllRoom from './pageEdu/AllRoom';
+import BoxNoti2 from './oanchisa/BoxNoti2';
 
 
 
 
 function App() {
   return (
-    // <Router>
-    //   <div>
-    //     <NavbarAdmin />
-    //     <HeaderAdmin />
-    //     <Routes>
-    //       <Route exact path="/ประกาศ" element={<AdminNoti />} />
-    //       <Route exact path="/กำหนดการ" element={<AdminTimeDate />} />
-    //       <Route exact path="/ข้อมูลผู้ใช้งาน" element={<AdminUser />} />
-    //     </Routes>
-    //   </div>
-    // </Router>
+    // <BoxNoti2/>
     <Router>
       <div>
-        <NavbarEdu />
-        <HeaderEdu />
+        <NavbarAdmin />
+        <HeaderAdmin />
         <Routes>
-          <Route exact path="/" element={<EduNoti />} />
-          <Route exact path="/ผลการลงทะเบียน" element={<RegisResults />} />
-          <Route exact path="/รายวิชาที่เปิดสอน" element={<CoursesTaught />} />
-          <Route exact path="/ห้องเรียนทั้งหมด" element={<AllRoom />} />
+          <Route exact path="/" element={<AdminNoti />} />
+          <Route exact path="/กำหนดการ" element={<AdminTimeDate />} />
+          <Route exact path="/ข้อมูลผู้ใช้งาน" element={<AdminUser />} />
         </Routes>
       </div>
     </Router>
+    // <Router>
+    //   <div>
+    //     <NavbarEdu />
+    //     <HeaderEdu />
+    //     <Routes>
+    //       <Route exact path="/" element={<EduNoti />} />
+    //       <Route exact path="/ผลการลงทะเบียน" element={<RegisResults />} />
+    //       <Route exact path="/รายวิชาที่เปิดสอน" element={<CoursesTaught />} />
+    //       <Route exact path="/ห้องเรียนทั้งหมด" element={<AllRoom />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
 
     
     // <div>
--- clients/src/pageAdmin/AdminNoti.js
+++ clients/src/pageAdmin/AdminNoti.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavbarAdmin from "../navbar/NavbarAdmin";
 import HeaderAdmin from "../navbar/HeaderAdmin";
+import BoxNoti2 from '../oanchisa/BoxNoti2';
 
 function AdminNoti() {
   return (
     <div>
       {/* <NavbarAdmin />
       <HeaderAdmin /> */}
+      <BoxNoti2/>
     </div>
   );
 }
