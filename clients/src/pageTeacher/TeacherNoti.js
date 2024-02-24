0c4d41b39b021217b6b336f88783daa5c67d6426
Phitchayp <157294602+Phitchayp@users.noreply.github.com>
2024-02-24 18:47:19 +0700

    components beam

--- clients/src/pageTeacher/TeacherNoti.js
+++ clients/src/pageTeacher/TeacherNoti.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import RegistrationResult from '../components/RegistrationResult';
 
 function TeacherNoti() {
   return (
     <div>
+      <RegistrationResult></RegistrationResult>
    
    
     </div>
