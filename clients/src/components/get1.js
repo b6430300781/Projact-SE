16ca5fc7053eb49b37b6f834472839b22369b265
mosuphattra <suphattrakhamphat45@gmail.com>
2024-03-02 19:13:39 +0700

    ข้อมูลผู้ใช้Admin

--- clients/src/components/get1.js
+++ clients/src/components/get1.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Axios from "axios";
+import bin from '..//assets/bin.png';
+import userIcon from '..//assets/userIcon.png';
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
@@ -15,18 +17,50 @@ const UserList = () => {
       });
   }, []); 
 
+  const handleDeleteUser = async (userId) => {
+    try {
+      await Axios.delete(`http://localhost:3001/usersaj/${userId}`);
+      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== userId));
+      // Assuming "setUsers" is the correct way to update your state after deletion
+    } catch (error) {
+      console.error('Error deleting data:', error);
+    }
+  };
+
   return (
     <div>
       <ul>
         {users.map((user, index) => (
-          <li key={index}>
-            <p>Email: {user.email}</p>
-            <p>Full Name: {user.name}</p>
-            {/* เพิ่มข้อมูลอื่น ๆ ตามต้องการ */}
-          </li>
+          <div key={index}>
+            <li style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginRight: '20px' }}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img
+                  style={{ width: '50px', height: '40px', marginRight: '10px' }}
+                  src={userIcon}
+                  alt="User Icon"
+                />
+                <div>
+                  <p>Email: {user.email}</p>
+                  <p>Full Name: {user.name}</p>
+                </div>
+              </div>
+              <img
+                style={{ width: '30px', height: '30px', cursor: 'pointer' }}
+                src={bin}
+                alt="Delete Icon"
+                onClick={() => {
+                  if (window.confirm('คุณแน่ใจหรือไม่ว่าต้องการลบข้อมูลนี้?')) {
+                    handleDeleteUser(user.id);
+                  }
+                }}
+              />
+            </li>
+            <hr style={{ width: '900px', color: 'white', margin: '10px auto' }} />
+          </div>
         ))}
       </ul>
     </div>
+    
   );
 };
 
