0e74dfc0341762c0aefe62d0e12d8a0584356695
Phitchayp <157294602+Phitchayp@users.noreply.github.com>
2024-03-06 20:04:22 +0700

    นาฟบาร์เฉยๆ

---
+++ clients/src/componentsEdu/CheckboxOpenCourse.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './CheckboxOpenCourse.css';
+
+
+function CheckboxOpenCourse() {
+    return (
+        <div>
+            <div className='turnleft-all'>
+                <div className='DateAdmin-text'>
+                    <h>เลือกรายวิชาที่สามารถเปิดสอน</h>
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+    );
+}
+
+export default CheckboxOpenCourse;
